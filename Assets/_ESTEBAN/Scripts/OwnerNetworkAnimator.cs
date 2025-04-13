9a5d582cd7da87fb805a00074f3a31c8e41e68b0
Esteban Maldonado, Developer Advocate <esteban.maldonado@unity3D.com>
2025-04-12 22:50:31 -0700

    added comments to the owner network animator

--- Assets/_ESTEBAN/Scripts/OwnerNetworkAnimator.cs
+++ Assets/_ESTEBAN/Scripts/OwnerNetworkAnimator.cs
@@ -1,5 +1,15 @@
 using Unity.Netcode.Components;
 
+/// <summary>
+/// A componet you add to an object to synchronize animations, when the object
+/// is owned by a device (ex. a player you control).
+/// 
+/// Note: Although this is an added component, and not part of the Netcode solution,
+///   how to easily add this code is mentioned in our documentation.
+///
+/// To learn more about network animators, got the following link:
+/// <see cref="https://docs-multiplayer.unity3d.com/netcode/current/components/networkanimator/"/>
+/// </summary>
 public class OwnerNetworkAnimator : NetworkAnimator
 {
     protected override bool OnIsServerAuthoritative()
