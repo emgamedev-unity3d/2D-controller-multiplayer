2a858bb5f15475a2281d1bb3cd0339de221f4116
Esteban Maldonado, Developer Advocate <esteban.maldonado@unity3D.com>
2025-04-14 11:27:09 +0400

    adding a complete version of the network manager

--- Assets/Scripts/HeyCameraLookAtMe.cs
+++ Assets/Scripts/HeyCameraLookAtMe.cs
@@ -1,10 +1,15 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using Unity.Netcode;
 
-public class HeyCameraLookAtMe : MonoBehaviour
+public class HeyCameraLookAtMe : MonoBehaviour //NetworkBehaviour
 {
     void Start()
     {
+        // TODO: uncomment to only update physics on owner's POV
+        //if (!IsOwner)
+        //    return;
+
         var cam = FindAnyObjectByType<CinemachineCamera>();
         cam.Target.TrackingTarget = transform;
     }
